db5bece282960a35ab360fdd85b313ae4802fb9c
MrFan <92414382+MrFanCode@users.noreply.github.com>
2025-01-30 12:48:30 +0800

    initialized

--- calculator.cpp
+++ calculator.cpp
@@ -2,11 +2,12 @@
 
 using namespace std;
 
-
+// Initializing/Declaring the function before using it
 void calculator();
 
 int main() {
 
+	// Calling the calculator function
 	calculator();
 
 	return 0;
@@ -15,12 +16,13 @@ int main() {
 
 void calculator() {
 
+	// Initializing variables
 	int operation;
-	double first_num, second_num, sum;
+	double first_num, second_num, sum; 
 
 	cout << "**********************************************" << endl;
 	cout << "WELCOME TO THE SIMPLE CALCULATOR" << endl;
-	
+	// This will lis the operation menu for the user to refer
 	cout << "1. Addition (+)" << endl;
 	cout << "2. Subtractrion (-)" << endl;
 	cout << "3. Multiplication (*)" << endl;
@@ -29,7 +31,7 @@ void calculator() {
 	cout << "Choose a operation from option above through (1-4): ";
 	cin >> operation;
 
-
+	// Handling the each operation
 	switch ( operation ) {
 		case 1:
 			cout << "Enter the first number: ";
@@ -67,7 +69,7 @@ void calculator() {
 			cout << "Enter the second number: ";
 			cin >> second_num;
 
-
+			// Check for zero division error and handle the error
 			if ( second_num != 0 ) {
 				sum = first_num / second_num;
 				cout << sum << endl;
@@ -76,12 +78,13 @@ void calculator() {
 			}
 			break;
 		default:
-			cout << "Please enter a valid value" << endl;
+			cout << "Please enter a valid value" << endl; // Return a message if there is no valid number entered
 			break;
 
 	}
 
 	cout << "*********************************************************";
+	// END PROGRAM
 
 	// Checking for the operation and handle the operation
 	//if ( operation == 1 ) {
